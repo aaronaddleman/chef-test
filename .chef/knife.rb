60b147f09e8403c5ab7d0cfcb05f7ee50d0f03ad
Aaron Addleman <Aaron_Addleman@gap.com>
2016-05-13 20:29:38 -0700

    chef server

---
+++ .chef/knife.rb
@@ -0,0 +1,15 @@
+current_dir = File.dirname(__FILE__)
+log_level                 :info
+log_location              STDOUT
+node_name                 "aaddleman"
+client_key                "#{current_dir}/aaddleman.pem"
+validation_client_name    "fullaccess-validator"
+validation_key            "#{current_dir}/fullaccess-validator.pem"
+chef_server_url           "http://192.168.0.10/organizations/fullaccess"
+syntax_check_cache_path   "#{ENV['HOME']}/.chef/syntaxcache"
+ssl_verify_mode           :verify_none
+cookbook_path             "#{current_dir}/../cookbooks"
+node_path                 "#{current_dir}/../nodes"
+role_path                 "#{current_dir}/../roles"
+data_bag_path             "#{current_dir}/../data_bags"
+environment_path          "#{current_dir}/../environments"
---
+++ cookbooks/chef-server/attributes/default.rb
@@ -0,0 +1,5 @@
+default['chef-server']['url'] = \
+  'https://packages.chef.io/stable/el/7/'\
+  'chef-server-core-12.6.0-1.el7.x86_64.rpm'
+default['chef-server']['sha1'] = '56bf780a9de2b1e7712d970d66e69b524b97a8dd'
+default['chef-server']['sha256'] = '5a36d0ffa692bf788e90315171582a758d4c5d8033a892dca9a81d3c03c44d14'
